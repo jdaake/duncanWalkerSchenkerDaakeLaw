3d90b719a2126f8657b5728da10ee70f3397603e
*J Daake* <jordandaake@JDaake-MacBook-Pro.local>
2020-08-25 21:09:41 -0500

    Added mobile  nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,7 +1,7 @@
 <script>
   import { onMount, onDestroy } from "svelte";
   import navStore from "../stores/nav-store.js";
-  let mobile = false;
+  let mobile = true;
   let homeIsActive;
   let aboutIsActive;
   let practiceIsActive;
@@ -51,18 +51,47 @@
     top: 25px;
     right: 25px;
   }
+  .mobile-nav {
+    visibility: hidden;
+  }
+
   @media (max-width: 991.98px) {
   }
 
-  @media (max-width: 578.98px) {
+  @media (max-width: 660px) {
+    .main-nav {
+      display: none !important;
+    }
+    .mobile-nav {
+      visibility: visible;
+      position: fixed;
+      width: 100%;
+      top: 0;
+      left: 0;
+      z-index: 1;
+      height: 50px;
+      background-color: rgb(65, 107, 176) !important;
+    }
+    .menu-icon {
+      position: fixed;
+      top: 5px;
+      left: 5px;
+      color: white;
+    }
+    .nav-logo {
+      max-width: 30px;
+      position: fixed;
+      top: 11px;
+      left: 90%;
+    }
   }
 </style>
 
-<nav class="uk-navbar-container" uk-navbar>
+<nav class="uk-navbar-container main-nav" uk-navbar>
   <a class="uk-navbar-item uk-logo" href="/" title="Home">
     <img class="nav-logo" src="./images/logos/dwsd-logo.jpg" alt="DSWD Logo" />
   </a>
-  <div class="uk-navbar-center uk-flex">
+  <div class="uk-navbar-center">
     <ul class="uk-navbar-nav">
       <li
         class={pageIsActive.activePage == 'home' ? 'uk-active' : ''}
@@ -85,39 +114,63 @@
         <a href="locations">Locations</a>
       </li>
     </ul>
-
   </div>
   <a
     class="facebook-logo"
     href="https://www.facebook.com/DucanWalkerSchenkerDaakePCLLO/"
-    rel="noreferrer"
     target="_blank"
     title="Visit our Facebook page">
     <span uk-icon="icon: facebook" />
   </a>
 </nav>
+
 <!-- Mobile Nav-->
-{#if mobile}
-  <button
-    class="uk-button uk-button-default uk-margin-small-right"
-    type="button"
-    uk-toggle="target: #offcanvas-usage">
-    Open
-  </button>
-  <div id="offcanvas-usage" uk-offcanvas>
+<nav class="mobile-nav">
+  <div>
+    <span
+      uk-icon="icon: menu"
+      uk-toggle="target: #offcanvas-slide"
+      ratio="2"
+      class="menu-icon" />
+    <a class="" href="/" title="Home">
+      <img
+        class="nav-logo"
+        src="./images/logos/dwsd-logo.jpg"
+        alt="DSWD Logo" />
+    </a>
+  </div>
+  <div id="offcanvas-slide" uk-offcanvas>
     <div class="uk-offcanvas-bar">
-
-      <button class="uk-offcanvas-close" type="button" uk-close />
-
-      <h3>Title</h3>
-
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
-      </p>
-
+      <ul class="uk-nav uk-nav-default">
+        <li
+          class={pageIsActive.activePage == 'home' ? 'uk-active' : ''}
+          title="Home">
+          <a href="/">Home</a>
+        </li>
+        <li
+          class={pageIsActive.activePage == 'about' ? 'uk-active' : ''}
+          title="View our Team">
+          <a href="about">About</a>
+        </li>
+        <li
+          class={pageIsActive.activePage == 'practice' ? 'uk-active' : ''}
+          title="View our Areas of Practices">
+          <a href="practice">Areas of Practice</a>
+        </li>
+        <li
+          class={pageIsActive.activePage == 'locations' ? 'uk-active' : ''}
+          title="View our Locations">
+          <a href="locations">Locations</a>
+        </li>
+        <li>
+          <a
+            href="https://www.facebook.com/DucanWalkerSchenkerDaakePCLLO/"
+            target="_blank"
+            title="Visit our Facebook page">
+            <span uk-icon="icon: facebook" />
+          </a>
+        </li>
+      </ul>
     </div>
   </div>
-{/if}
+</nav>
